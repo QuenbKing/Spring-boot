dc1974d4aad030ae441183e2f315666ff70240b8
QuenbKing <126509056+QuenbKing@users.noreply.github.com>
2023-11-08 22:35:50 +0500

    Add files via upload

---
+++ springboot/springboot/src/main/java/me/ulearn/springboot/controller/GameController.Initial.java
@@ -0,0 +1,33 @@
+package me.ulearn.springboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/games")
+public class OldGameController {
+
+    private ... getGames(...) {
+        throw new UnsupportedOperationException();
+    }
+
+
+    private ... getGameById(...) {
+        throw new UnsupportedOperationException();
+    }
+
+
+    private ... postGame(...) {
+        throw new UnsupportedOperationException();
+    }
+
+
+    private ... postGame(...) {
+        throw new UnsupportedOperationException();
+    }
+
+
+    private ... deleteGame(...) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ springboot/springboot/src/main/java/me/ulearn/springboot/entity/Game.Initial.java
@@ -0,0 +1,7 @@
+package me.ulearn.springboot.entity;
+
+public class OldGame {
+    private ... id;
+    private ... name;
+    private ... developer;
+}
